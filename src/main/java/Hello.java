210cc1a3248d81420ac797f7b199c8aa113d9b7d
Bekirsengul <bekirsengul06@gmail.com>
2021-06-11 11:37:59 +0200

    added moree hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("local Repo is already published to GitHub");
+        System.out.println("doing more work");
     }
 }
